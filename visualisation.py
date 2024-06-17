05abe0c6c931c56752f4a121a4d038f1a7743d61
Jan Kellershohn <kellershohn@fh-aachen.de>
2024-06-17 10:31:06 +0200

    Added some folium formatting

--- visualisation.py
+++ visualisation.py
@@ -1,32 +1,42 @@
 import pandas as pd
 import geopandas as gpd
+import folium
 from bikeability_config import USE_ACCIDENTS, EXPORT_PATH
 
 def create_suitability_visualisation(edges: pd.DataFrame):
     edges_for_vis = gpd.GeoDataFrame(edges, crs="EPSG:25832")
     edges_for_vis = edges_for_vis[~edges_for_vis.geometry.isna()]
+    edges_for_vis = edges_for_vis[['osmid', 'name', 'highway', 'suitability_modifier', 'score_surface', 'score_separation', "accident_count", 'geometry']]
     style_kwds = {"weight": 3}
 
+    scores_surface = folium.Map(tiles = "CartoDB positron")
     scores_surface = edges_for_vis.explore(column = "score_surface", 
                                             cmap = "viridis", 
                                             vmin = 0, 
                                             vmax = 1,
-                                            style_kwds = style_kwds)
+                                            style_kwds = style_kwds,
+                                            m = scores_surface)
     scores_surface.save(f"{EXPORT_PATH}/surface.html")
     
     
+    scores_separation = folium.Map(tiles = "CartoDB positron")
     scores_separation = edges_for_vis.explore(column = "score_separation", 
                                               cmap = "viridis", 
                                               vmin = 0, 
                                               vmax = 1,
-                                              style_kwds = style_kwds)
+                                              style_kwds = style_kwds,
+                                              m = scores_separation)
     scores_separation.save(f"{EXPORT_PATH}/separation.html")
     
+    
+    
+    suitability_score = folium.Map(tiles = "CartoDB positron")
     suitability_score = edges_for_vis.explore(column = "suitability_modifier",
                                       cmap = "viridis",
                                       vmin = 0, 
                                       vmax = 1,
-                                      style_kwds = style_kwds)
+                                      style_kwds = style_kwds,
+                                      m = suitability_score)
     suitability_score.save(f"{EXPORT_PATH}/suitability.html")
 
     if USE_ACCIDENTS:
@@ -36,11 +46,11 @@ def create_suitability_visualisation(edges: pd.DataFrame):
                                           vmax = 10)
         accident_count.save(f"{EXPORT_PATH}/accidents.html")
     
-        score_accident = edges_for_vis.explore(column = "score_accident",
-                                          cmap = "viridis",
-                                          vmin = 0, 
-                                          vmax = 5)
-        score_accident.save(f"{EXPORT_PATH}/accidents_score.html")
+        # score_accident = edges_for_vis.explore(column = "score_accident",
+        #                                   cmap = "viridis",
+        #                                   vmin = 0, 
+        #                                   vmax = 5)
+        # score_accident.save(f"{EXPORT_PATH}/accidents_score.html")
 
 def create_building_visualisation(buildings:gpd.GeoDataFrame):
     buildings_for_vis = buildings[["osmid", "geometry", "centroid", "node", "building", "full_score"]]
