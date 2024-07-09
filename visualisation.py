d9dbd7e7d22f39c12b1cd227d49c67d9d1761c1c
Jan Kellershohn <kellershohn@fh-aachen.de>
2024-07-09 09:58:10 +0200

    Added full german translation of maps

--- visualisation.py
+++ visualisation.py
@@ -15,7 +15,8 @@ def create_suitability_visualisation(edges: pd.DataFrame):
                                   "score_surface": "Wertung Oberflächenqualität",
                                   "score_separation": "Wertung Trennung",
                                   "accident_count": "Anzahl Unfälle (3 Jahre)",
-                                  "highway": "OSM Highwaytyp"})
+                                  "highway": "OSM Highwaytyp"}, 
+                         inplace = True)
 
     scores_surface = folium.Map(tiles = "CartoDB positron")
     scores_surface = edges_for_vis.explore(column = "Wertung Oberflächenqualität", 
@@ -61,15 +62,28 @@ def create_suitability_visualisation(edges: pd.DataFrame):
         # score_accident.save(f"{EXPORT_PATH}/accidents_score.html")
 
 def create_building_visualisation(buildings:gpd.GeoDataFrame):
-    buildings_for_vis = buildings[["osmid", "geometry", "centroid", "node", "building", "full_score"]]
-    buildings_vis = buildings_for_vis.explore(column = "full_score",
+    buildings_for_vis = buildings[["osmid", "node", "building", "score", "geometry", "centroid"]]
+    buildings_for_vis.rename(columns = {"osmid": "OSM ID",
+                                        "node": "Zugehöriger Knoten",
+                                        "building": "Gebäudetyp",
+                                        "score": "Score"},
+                             inplace = True)
+    buildings_vis = folium.Map(tiles = "CartoDB positron")
+    buildings_vis = buildings_for_vis.explore(column = "Score",
                                       cmap = "viridis",
                                       vmin = 0, 
-                                      vmax = 1)
+                                      vmax = 1,
+                                      m = buildings_vis)
     buildings_vis.save(f"{EXPORT_PATH}/buildings.html")
     
 def create_POI_visualisation(POIs: gpd.GeoDataFrame):
     POIs_for_vis = POIs[["name", "osmid", "geometry", "node", "POI_category"]]
+    POIs_for_vis.rename(columns = {"osmid": "OSM ID",
+                                   "name": "Name",
+                                   "node": "Zugehöriger Knoten",
+                                   "POI_category": "Kategorie"},
+                        inplace = True)
     POIs_for_vis = POIs_for_vis[POIs_for_vis.POI_category != "none"]
-    POIs_vis = POIs.explore(column = "POI_category")
+    POIs_vis = folium.Map(tiles = "CartoDB positron")
+    POIs_vis = POIs.explore(column = "POI_category", m = POIs_vis)
     POIs_vis.save(f"{EXPORT_PATH}/POIs.html")
